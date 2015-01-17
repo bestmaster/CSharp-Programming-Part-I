1d8c8843c49c1554e64ce2b0081cf5b56ab6d312
bestmaster <denislav@denislav.com>
2015-01-17 17:27:02 +0200

    Homework Introduction to Programming

---
+++ 1.Introduction to Programming/15.AgeAfter10Years/AgeAfter10Years.cs
@@ -0,0 +1,17 @@
+﻿//Problem 15.* Age after 10 Years
+//
+//    Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
+
+using System;
+
+    class AgeAfter10Years
+    {
+        static void Main()
+        {
+            string age = null;
+            Console.WriteLine("How old are you?");
+            Console.Write(">> ");
+            age = Console.ReadLine();
+            Console.WriteLine("After 10 years you'll be {0} years old.", int.Parse(age) + 10);
+        }
+    }
